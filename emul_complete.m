e5f1285ecc19292c85a8d643ecec3c1d687359e6
Dario Domingo <dariodomingo89@gmail.com>
2019-02-17 15:40:36 +0000

    Update emul_complete.m

--- emul_complete.m
+++ emul_complete.m
@@ -1,37 +1,27 @@
-% Given the output of 'emulation_PCscores.m', this function computes temperature emulated mean and covariance for a set of Nloc locations.
-% Values of the PCs at the N locations are provided in PC_Val. The average
-% of the original simulations at these locations is provided in Mn_Val.
-% Either a full covariance or just the variance at the times corresponding
-% to the rows of M_Pc is output, according to whether 'cov' or 'var' is
-% specified as last input.
-% The variance from the components not used in the linear combination is 
-% added to the variance arising from the linear combination.
+% Given the output of 'emulation_PCscores.m', this function computes temperature emulated mean and covariance for a set
+% of Nloc locations. Values of the PCs at the N locations are provided in PC_Val. The average of the original simulations 
+% at these locations is provided in Mn_Val.
+% Either a full covariance or just the variance at the times corresponding to the rows of M_Pc is output, according to
+% whether 'cov' or 'var' is specified as last input.
+% The variance from the unused components is added to the variance arising from the linear combination.
 %
 % INPUTS:
-% M_Pc:   Txn matrix (n>=r). In j-th column, emulated mean of the
-%         coefficient of the j-th PC.
-% Cov_Pc: Usually, TxTxr. Cov(:,:,j) represents the covariance matrix of
-%         the coefficient of the j-th PC. (last dimension can be >r)
-%         If input var_cov is the string 'var', then Cov_Pc should be a Txr
-%         matrix, with only emulated variances in each column.
-% PC_Val: Nloc x n matrix. In each row, values of the n PCs at the location
-%         corresponding to that row. There are Nloc locations.
-% Mn_Val: Nloc x 1, with average value of the original simulation outputs
-%         at each of the N locations. This will be added to the linear
-%         combination of the columns of Pc_Val.
-% Std_PCA: Row or column vector of length n, with standard deviation of PCs
-%         (from eigenvalue decomposition).
-% r:      number of PCs to use in linear combinations
-% var_cov: a string, either 'var' or 'cov'. Accordingly, only variance or
-%          full covariance will be computed as output Cov.
+% M_Pc:    Txn matrix (n>=r). In j-th column, emulated mean of the coefficient of the j-th PC.
+% Cov_Pc:  Usually, TxTxr. Cov(:,:,j) represents the covariance matrix of the coefficient of the j-th PC. 
+%          (last dimension can be >r)
+%          If input var_cov = 'var', then Cov_Pc should be a Txr matrix, with only emulated variances in each column.
+% PC_Val:  Nloc x n matrix. In each row, values of the n PCs at the location corresponding to that row.
+% Mn_Val:  Nloc x 1, with average value of the original simulation outputs at each of the N locations. This will be added 
+%          to the linear combination of the columns of Pc_Val.
+% Std_PCA: Row or column vector of length n, with standard deviation of PCs (from eigenvalue decomposition).
+% r:       number of PCs to use in linear combinations
+% var_cov: a string, either 'var' or 'cov'. Accordingly, only variance or full covariance will be computed as output Cov.
 %
 %
 % OUTPUTS:
-% M: T x Nloc, with emulated mean temperature at time i and location j
-%    stored in M(i,j).
+% M: T x Nloc, with emulated mean temperature at time i and location j stored in M(i,j).
 %
-% Second output changes according to whether 'var' or 'cov' is provided as
-% var_cov input.
+% Second output changes according to whether 'var' or 'cov' is provided as var_cov input.
 %
 % If 'var', then:
 %     VarCov: T x Nloc. As M, but with variances rather than means.
@@ -63,7 +53,7 @@ if strcmp(var_cov, 'cov') && length(size(Cov_Pc))~=3
 end
 
 
-%% General variables and reshaping
+% General variables and reshaping
 
 n = length(Std_PCA);   % total number of PCs
 T = size(M_Pc,1);      % length of vector containing times
@@ -73,7 +63,7 @@ Std_PCA = reshape(Std_PCA, [1,n]);
 M_Pc = M_Pc(:, 1:r);   % Txr
 
 
-%% Building full covariance (or just variance) matrix of PCs, considering "constant" variance for non-used PCs
+% Building full covariance (or just variance) matrix of PCs, considering "constant" variance for non-used PCs
 
 if strcmp(var_cov, 'var')
     Full_PC_Var = zeros(T,n);    % Txn
@@ -93,7 +83,7 @@ else
 end
 
 
-%% Computing Emulator mean and variances
+% Computing Emulator mean and variances
 
 M = ones(T,1)*Mn_Val' + (M_Pc*PC_Val(:, 1:r)'); % TxNloc + (Txr)x(rxNloc)
 Squared_PC = (PC_Val').^2;     % n x Nloc
